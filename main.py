5eefe6a29db3f33f3013041205939ec6449f28bc
Ram Babu <ram.kottapally@gmail.com>
2021-04-06 13:07:43 +0530

    Add 404.html

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import (Flask, escape, render_template, request, send_file,
-                   send_from_directory, url_for)
+                   send_from_directory, url_for, abort)
 
 app = Flask(__name__)
 
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html', title='404'), 404
 
 @app.route('/<state_code>', methods=['GET', 'POST'])
 def index(state_code):
@@ -19,6 +23,8 @@ def index(state_code):
         'MZ': 'Mizoram', 'NL': 'Nagaland', 'OR': 'Odisha', 'PB': 'Punjab', 'PY': 'Puducherry', 'RJ': 'Rajasthan', 'SK': 'Sikkim',
         'TG': 'Telangana', 'TN': 'Tamil Nadu', 'TR': 'Tripura', 'UP': 'Uttar Pradesh', 'UT': 'Uttarakhand', 'WB': 'West Bengal'
     }
+    if state_code not in state_code_dict:
+        abort(404)
     return render_template('state_data.html', state=state_code_dict[state_code], state_id=state_code)
 
 
