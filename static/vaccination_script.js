e56804bb834257a160ac6e616019dfc54638dcf2
Ram Babu <ram.kottapally@gmail.com>
2021-07-28 17:05:50 +0530

    Add preloader when searching

--- static/vaccination_script.js
+++ static/vaccination_script.js
@@ -2,13 +2,20 @@ $(document).ready(function () {
     var today;
 
     async function getdata(pin_code, date) {
+        $('#vaccine_table').css("visibility", "hidden");
+        $('#preload_vaccine').css("display", "block");
+        $('#preload_vaccine').css("visibility", "visible");
+        
         const url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=" + pin_code + "&date=" + date;
         const response = await fetch(url);
         var data = await response.json();
         //console.log(data['sessions'].length);
         $("#vaccine_table > tbody").empty();
         if (data['sessions'].length == 0) {
+            $('#preload_vaccine').css("display", "block");
+            $('#preload_vaccine').css("visibility", "visible");
             alert('No Data Available');
+            $('#preload_vaccine').css("display", "none");
         }
         else {
             //$("#vaccine_table > tbody").empty();
@@ -43,12 +50,15 @@ $(document).ready(function () {
                 let state = temp['state_name'];
                 let capacity = temp['available_capacity'];
 
-                var row = '<tr><td>' + centre_name + ' (' + centre_id + ')' + '</td><td class="vaccine_name ' + cls_vaccine + '">' + vaccine_name + '<br><hr><small class="avl_doses">Doses: '+ capacity +'</small></td><td>' + time_slot + '</td><td>' + address + '</td><td>' + fee_type + '</td></tr>';
+                var row = '<tr><td>' + centre_name + ' (' + centre_id + ')' + '</td><td class="vaccine_name ' + cls_vaccine + '">' + vaccine_name + '<br><hr><small class="avl_doses">Doses: ' + capacity + '</small></td><td>' + time_slot + '</td><td>' + address + '</td><td>' + fee_type + '</td></tr>';
                 $('#heading').text('Vaccination Centres - ' + pincode + " | " + state);
                 $('#vaccine_table').append(row);
             }
-            
+            $('#preload_vaccine').css("visibility", "hidden");
+            $('#preload_vaccine').css("display", "none");
+            $('#vaccine_table').css("visibility", "visible");
         }
+
     };
 
 
@@ -63,6 +73,7 @@ $(document).ready(function () {
         else {
             today = moment(new Date()).format('DD-MM-YYYY');
             getdata(pin_code, today);
+
         }
 
     });
